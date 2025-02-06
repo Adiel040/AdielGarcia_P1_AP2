e6294d61c9ad67959b60442e43a9ecf8cd62c6d3
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-05 23:48:37 -0400

    Creando las entidades

--- app/src/main/java/edu/ucne/adielgarcia_p1_ap2/data/local/entities/SistemaEntity.kt
+++ app/src/main/java/edu/ucne/adielgarcia_p1_ap2/data/local/entities/SistemaEntity.kt
@@ -3,9 +3,10 @@ package edu.ucne.adielgarcia_p1_ap2.data.local.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Entities")
-data class Entity(
-    //Cambiar en el examen
+@Entity(tableName = "Sistema")
+data class SistemaEntity(
+
     @PrimaryKey
-    val entityId: Int?
+    val SistemaId: Int?,
+    val Nombre: String
 )
