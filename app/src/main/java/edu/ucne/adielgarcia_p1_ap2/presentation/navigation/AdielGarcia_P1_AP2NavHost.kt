c672c10a2fb9dfd6b8e9173fc41576f41d42f0a2
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-05 23:47:22 -0400

    Agregando algunas funciones , para que navegue correctamente

--- app/src/main/java/edu/ucne/adielgarcia_p1_ap2/presentation/navigation/AdielGarcia_P1_AP2NavHost.kt
+++ app/src/main/java/edu/ucne/adielgarcia_p1_ap2/presentation/navigation/AdielGarcia_P1_AP2NavHost.kt
@@ -4,14 +4,35 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.adielgarcia_p1_ap2.presentation.entities.SistemaListScreen
+import edu.ucne.adielgarcia_p1_ap2.presentation.entities.SistemaScreen
 
 @Composable
 fun AdielGarcia_P1_AP2NavHost(navHostController: NavHostController){
+
     NavHost(
         startDestination = Screen.EntityList,
         navController = navHostController
     ) {
-        composable<Screen.EntityList> {  }
-        composable<Screen.EntityScreen> {  }
+        composable<Screen.EntityList> {
+            SistemaListScreen(
+            onAddClick = {
+                navHostController.navigate(Screen.EntityScreen(entityId = 0))
+            },
+            editarSistema = {
+                navHostController.navigate(Screen.EntityScreen(entityId = it))
+            },
+            )
+        }
+        composable<Screen.EntityScreen> {
+            val id = it.toRoute<Screen.EntityScreen>().entityId
+            SistemaScreen(
+                id = id,
+                goBack = {
+                    navHostController.popBackStack()
+                }
+            )
+        }
     }
 }
