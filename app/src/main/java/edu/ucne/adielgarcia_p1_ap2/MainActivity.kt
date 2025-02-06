c87b2c7fa8253395b4b3c68b3db54dcbe9880fb1
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-05 23:48:04 -0400

    Agregando libreria para la navegacion

--- app/src/main/java/edu/ucne/adielgarcia_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/adielgarcia_p1_ap2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.adielgarcia_p1_ap2.presentation.navigation.AdielGarcia_P1_AP2NavHost
 import edu.ucne.adielgarcia_p1_ap2.ui.theme.AdielGarcia_P1_AP2Theme
 
 @AndroidEntryPoint
@@ -22,10 +25,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             AdielGarcia_P1_AP2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        val navHostController = rememberNavController()
+                        AdielGarcia_P1_AP2NavHost(navHostController)
+                    }
                 }
             }
         }
