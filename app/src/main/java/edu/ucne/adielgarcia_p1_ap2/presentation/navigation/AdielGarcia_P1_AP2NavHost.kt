8193b49e80afba8b45f70af72bcb31fe5371d846
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-12 13:23:01 -0400

    Corrigiendo la navegation

--- app/src/main/java/edu/ucne/adielgarcia_p1_ap2/presentation/navigation/AdielGarcia_P1_AP2NavHost.kt
+++ app/src/main/java/edu/ucne/adielgarcia_p1_ap2/presentation/navigation/AdielGarcia_P1_AP2NavHost.kt
@@ -1,38 +1,66 @@
 package edu.ucne.adielgarcia_p1_ap2.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import edu.ucne.adielgarcia_p1_ap2.presentation.entities.SistemaListScreen
-import edu.ucne.adielgarcia_p1_ap2.presentation.entities.SistemaScreen
+import edu.ucne.adielgarcia_p1_ap2.presentation.Sistemas.DeleteSistemaScreen
+import edu.ucne.adielgarcia_p1_ap2.presentation.Sistemas.EditSistemaScreen
+import edu.ucne.adielgarcia_p1_ap2.presentation.Sistemas.SistemaListScreen
+import edu.ucne.adielgarcia_p1_ap2.presentation.Sistemas.SistemaScreen
 
 @Composable
 fun AdielGarcia_P1_AP2NavHost(navHostController: NavHostController){
+    val scope = rememberCoroutineScope()
 
-    NavHost(
-        startDestination = Screen.EntityList,
-        navController = navHostController
-    ) {
-        composable<Screen.EntityList> {
+    NavHost(navController = navHostController, startDestination = Screen.SistemaList,)
+    {
+        composable<Screen.SistemaList> {
             SistemaListScreen(
-            onAddClick = {
-                navHostController.navigate(Screen.EntityScreen(entityId = 0))
-            },
-            editarSistema = {
-                navHostController.navigate(Screen.EntityScreen(entityId = it))
-            },
-            )
-        }
-        composable<Screen.EntityScreen> {
-            val id = it.toRoute<Screen.EntityScreen>().entityId
-            SistemaScreen(
-                id = id,
-                goBack = {
-                    navHostController.popBackStack()
+                scope = scope,
+                onCreateSistema = {
+                    navHostController.navigate(Screen.SistemaScreen(0))
+                },
+                onEditSistema = { sistema ->
+                    navHostController.navigate(Screen.EditScreen(sistema))
+                },
+                onDeleteSistema = { sistema ->
+                    navHostController.navigate(Screen.DeleteScreen(sistema))
                 }
             )
         }
+
+        composable<Screen.SistemaScreen> {
+            val args = it.toRoute<Screen.SistemaScreen>()
+            SistemaScreen(
+                goBack = {
+                    navHostController.navigateUp()
+                }
+            )
+        }
+
+        composable<Screen.EditScreen> {
+            val args = it.toRoute<Screen.EditScreen>()
+            EditSistemaScreen(
+                sistemaId = args.sistemaId,
+                goBack = {
+                    navHostController.navigateUp()
+                }
+            )
+        }
+
+        composable<Screen.DeleteScreen> {
+            val args = it.toRoute<Screen.DeleteScreen>()
+            DeleteSistemaScreen(
+                sistemaId = args.sistemaId,
+                goBack = {
+                    navHostController.navigateUp()
+                }
+            )
+        }
+
+
     }
 }
